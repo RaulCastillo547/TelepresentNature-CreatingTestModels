cba0964aa6d162068ae994cc4bcbc1cc284e1867
Raul Castillo <111318251+RaulCastillo547@users.noreply.github.com>
2024-08-13 19:13:19 -0400

    Implemented AR Temperature Model

--- auxiliaries.py
+++ auxiliaries.py
@@ -1,4 +1,5 @@
 import datetime
+import math
 import random
 
 import numpy as np
@@ -207,6 +208,197 @@ class WindowGenerator():
             self._example = result
         return result
 
+class TemperatureWindow():
+    def __init__(self, input_width, label_width, shift,
+                 cv_name, label_columns=None):
+        
+        # Derive original data
+        self.url_name = cv_name
+        self.orig_df = pd.read_csv(f'CSVFiles/CleanCSV/{cv_name}.csv')
+        self.orig_df['timestamp'] = pd.DatetimeIndex(self.orig_df['timestamp'])
+
+        self.date_time = pd.to_datetime(self.orig_df.pop('timestamp'))
+        self.orig_df.pop('longitude')
+        self.orig_df.pop('latitude')
+        self.orig_df.pop('altitude')
+
+        # Split data        
+        n = len(self.orig_df)
+        self.train_df = self.orig_df[0:int(n*0.7)]
+        self.val_df = self.orig_df[int(n*0.7):]
+        self.test_df = self.orig_df[int(n*0.9):]
+        self.edge_df = self.test_df.iloc[-(input_width + shift):]
+    
+        # Normalize data
+        self.train_mean = self.train_df.mean(numeric_only=True)
+        self.train_std = self.train_df.std(numeric_only=True)
+        
+        self.train_df = (self.train_df - self.train_mean) / self.train_std
+        self.val_df = (self.val_df - self.train_mean) / self.train_std
+        self.test_df = (self.test_df - self.train_mean) / self.train_std
+        self.edge_df = (self.edge_df - self.train_mean) / self.train_std
+
+        # Work out label column indices
+        self.label_columns = label_columns
+        if label_columns is not None:
+            self.label_columns_indices = {name: index for index, name in enumerate(label_columns)}
+        
+        self.column_indices = {name: index for index, name in enumerate(self.train_df.columns)}
+
+        # Work out the window parameters.
+        self.input_width = input_width
+        self.label_width = label_width
+        self.shift = shift
+
+        self.total_window_size = input_width + shift
+
+        # Slicing input and label widths
+        self.input_slice = slice(0, input_width)
+        self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+        self.label_start = self.total_window_size - self.label_width
+        self.label_slice = slice(self.label_start, None)
+        self.label_indices = np.arange(self.total_window_size)[self.label_slice]
+
+    def create_df_extension(self, species, model=None, number_of_out_steps=1):
+        # Load edge data
+        base_df = self.orig_df
+        add_on_data = []
+
+        # Run model
+        _, input_data = next(iter(self.edge_data))
+        input_data = input_data.numpy()
+
+        for i in range(number_of_out_steps):
+            if (model is not None):
+                input_data = model(input_data).numpy()
+            else:
+                input_data = np.array([input_data[-1][-1] for _ in range(self.label_width)]).reshape((1, 100, 1))
+            
+            add_on_data += input_data[0].tolist()
+        
+        # Define modeled and unmodeled datapoints
+        base_df['timestamp'] = self.date_time
+        base_df['modeled'] = False
+
+        add_on_df = pd.DataFrame(add_on_data, columns=self.edge_df.columns)*self.train_std + self.train_mean
+
+        start = max(self.date_time)
+        add_on_time_index = list()
+
+        assert species == 'Moose' or species == 'Deer'
+        if (species == 'Moose'):
+            for i in range(len(add_on_df)):
+                add_on_time_index.append(start + datetime.timedelta(hours=3*(i+1)))
+        elif (species == 'Deer'):
+            for i in range(len(add_on_df)):
+                add_on_time_index.append(start + datetime.timedelta(hours=4*(i+1)))
+        
+        add_on_df['timestamp'] = add_on_time_index
+        add_on_df['modeled'] = True
+
+        # Merge base_df and add_on_df and print out result
+        full_df = pd.concat([base_df, add_on_df], ignore_index=True)
+        full_df.set_index('timestamp', inplace=True)
+        return full_df
+    
+    def __repr__(self):
+        return '\n'.join([
+            f'Total window size: {self.total_window_size}',
+            f'Input indices: {self.input_indices}',
+            f'Label indices: {self.label_indices}',
+            f'Label column name(s): {self.label_columns}'])
+
+    def split_window(self, features):
+        inputs = features[:, self.input_slice, :]
+        labels = features[:, self.label_slice, :]
+
+        if self.label_columns is not None:
+            labels = tf.stack(
+                [labels[:,:, self.column_indices[name]] for name in self.label_columns],
+                axis = -1)
+        
+        inputs.set_shape([None, self.input_width, None])
+        labels.set_shape([None, self.label_width, None])
+
+        return inputs, labels
+    
+    def plot(self, model=None, plot_col = 'external-temperature', max_subplots=3):
+        inputs, labels = self.example
+        
+        plt.figure(figsize=(12, 8))
+        
+        plot_col_index = self.column_indices[plot_col]
+        
+        max_n = min(max_subplots, len(inputs))
+        
+        for n in range(max_n):
+            plt.subplot(max_n, 1, n+1)
+            plt.ylabel(f'{plot_col} [normed]')
+            plt.plot(self.input_indices, inputs[n, :, plot_col_index],
+                        label='Inputs', marker='.', zorder=-10)
+
+            if self.label_columns:
+                label_col_index = self.label_columns_indices.get(plot_col, None)
+            else:
+                label_col_index = plot_col_index
+
+            if label_col_index is None:
+                continue
+
+            plt.scatter(self.label_indices, labels[n, :, label_col_index],
+                        edgecolors='k', label='Labels', c='#2ca02c', s=64)
+            if model is not None:
+                predictions = model(inputs)
+                plt.scatter(self.label_indices, predictions[n, :, label_col_index],
+                            marker='X', edgecolors='k', label='Predictions',
+                            c='#ff7f0e', s=64)
+
+            if n == 0:
+                plt.legend()
+
+        plt.xlabel('Time [h]')
+    
+    def make_dataset(self, data, n = None):
+        data = np.array(data, dtype=np.float32)
+        ds = tf.keras.utils.timeseries_dataset_from_array(
+            data=data,
+            targets=None,
+            sequence_length=self.total_window_size,
+            sequence_stride=1,
+            shuffle=True,
+            seed=n,
+            batch_size=32,)
+        ds = ds.map(self.split_window)
+
+        return ds
+    
+    @property
+    def train(self):
+        return self.make_dataset(self.train_df)
+    
+    @property
+    def val(self):
+        return self.make_dataset(self.val_df)
+    
+    @property
+    def test(self):
+        return self.make_dataset(self.test_df)
+    
+    @property
+    def edge_data(self):
+        return self.make_dataset(self.edge_df)
+    
+    @property
+    def example(self):
+        result = getattr(self, '_example', None)
+        if result is None:
+            result = next(iter(self.train))
+
+            self._example = result
+        return result
+
+
 class RegressiveWindow():
     def __init__(self, csv_name):
         # Load and add month and day values
@@ -239,6 +431,11 @@ class RegressiveWindow():
         self.train_label = self.train_label.reshape((self.train_label.shape[0], 1, self.train_label.shape[1]))
         self.test_label = self.test_label.reshape((self.test_label.shape[0], 1, self.test_label.shape[1]))
 
+        # Run autoregressive model for temperature predictions
+        self.temperature_window = TemperatureWindow(input_width=OUT_STEPS, label_width=OUT_STEPS, shift=OUT_STEPS, cv_name=csv_name)
+        self.autoregressive_model = FeedBack(units=32, out_steps=OUT_STEPS, num_vars=1)
+        compile_and_fit(self.autoregressive_model, self.temperature_window)
+
     def model_compilation_and_fitting(self, model, patience=2):
         model.compile(loss=tf.keras.losses.MeanSquaredError(),
                       optimizer=tf.keras.optimizers.Adam(),
@@ -262,7 +459,7 @@ class RegressiveWindow():
         }
         
         # Define function to get temperature values
-        avg_temp = self.orig_df.groupby(['month', 'day']).mean()['external-temperature']
+        predicted_temp = self.temperature_window.create_df_extension(species, self.autoregressive_model, math.ceil(out_steps/self.temperature_window.label_width))
 
         for i in range(out_steps):
             # Get current date
@@ -273,7 +470,7 @@ class RegressiveWindow():
                 curr_date = max(self.timeline) + datetime.timedelta(hours=4*(i+1))
 
             # Derive input variables
-            external_temp = avg_temp[curr_date.month][curr_date.day]
+            external_temp = predicted_temp[predicted_temp['modeled'] == True].iloc[i, 0]
             month = curr_date.month
             day = curr_date.day
 
